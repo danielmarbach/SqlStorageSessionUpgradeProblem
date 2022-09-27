73774509839695729c0113d1462b3dc08e83bc60
danielmarbach <daniel.marbach@openplace.net>
2022-09-27 10:28:36 +0200

    Revert "Create multiple endpoints"
    This reverts commit ba7fcc49cec03b8ddb58b7bdc6fe2218dce7a6de.

--- Program.cs
+++ Program.cs
@@ -7,48 +7,47 @@ using SimpleInjector.Lifestyles;
 using Microsoft.Data.SqlClient;
 using SqlUpgradeRepro;
 
-async Task<IEndpointInstance> Create(string endpointName)
-{
-    var container = new Container();
-    container.Options.DefaultScopedLifestyle = new AsyncScopedLifestyle();
-    container.Options.AllowOverridingRegistrations = true;
-    container.Options.AutoWirePropertiesImplicitly();
-    container.Register<MyRepository>(Lifestyle.Scoped);
+Console.WriteLine("Hello, World!");
 
-    var endpointConfiguration = new EndpointConfiguration(endpointName);
-    endpointConfiguration.EnableInstallers();
+var container = new Container();
+container.Options.DefaultScopedLifestyle = new AsyncScopedLifestyle();
+container.Options.AllowOverridingRegistrations = true;
+container.Options.AutoWirePropertiesImplicitly();
+container.Register<MyRepository>(Lifestyle.Scoped);
+
+var endpointConfiguration = new EndpointConfiguration("SqlUpgradeRepro");
+endpointConfiguration.EnableInstallers();
 
 // var transport = endpointConfiguration.UseTransport<LearningTransport>();
 // transport.StorageDirectory(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, ".learningtransport"));
 
-    var transport = endpointConfiguration.UseTransport<RabbitMQTransport>();
-    transport.ConnectionString("host=localhost");
-    transport.UseConventionalRoutingTopology(QueueType.Quorum);
+var transport = endpointConfiguration.UseTransport<RabbitMQTransport>();
+transport.ConnectionString("host=localhost");
+transport.UseConventionalRoutingTopology(QueueType.Quorum);
 
-    var persistence = endpointConfiguration.UsePersistence<SqlPersistence>();
-    var connection =
-        @"Server=localhost,1433;Initial Catalog=SqlUpgradeRepro;User Id=sa;Password=yourStrong(!)Password;encrypt=false";
-    persistence.SqlDialect<SqlDialect.MsSqlServer>();
-    persistence.ConnectionBuilder(
-        connectionBuilder: () => { return new SqlConnection(connection); });
+var persistence = endpointConfiguration.UsePersistence<SqlPersistence>();
+var connection = @"Server=localhost,1433;Initial Catalog=SqlUpgradeRepro;User Id=sa;Password=yourStrong(!)Password;encrypt=false";
+persistence.SqlDialect<SqlDialect.MsSqlServer>();
+persistence.ConnectionBuilder(
+    connectionBuilder: () =>
+    {
+        return new SqlConnection(connection);
+    });
 
-    endpointConfiguration.UseContainer<SimpleInjectorBuilder>(
-        customizations => { customizations.UseExistingContainer(container); });
+endpointConfiguration.UseContainer<SimpleInjectorBuilder>(
+    customizations =>
+    {
+        customizations.UseExistingContainer(container);
+    });
 
-    SqlHelper.EnsureDatabaseExists(connection);
+SqlHelper.EnsureDatabaseExists(connection);
 
-    var endpointInstance1 = await Endpoint.Start(endpointConfiguration);
-    return endpointInstance1;
-}
+var endpointInstance = await Endpoint.Start(endpointConfiguration);
 
-Console.WriteLine("Hello, World!");
-
-var endpointInstance1 = await Create("SqlUpgradeRepro");
-var endpointInstance2 = await Create("SqlUpgradeRepro2");
-await endpointInstance1.SendLocal(new MyMessage());
+await endpointInstance.SendLocal(new MyMessage());
 
 Console.WriteLine("Press any key to exit");
 Console.ReadKey();
 
-await endpointInstance1.Stop();
-await endpointInstance2.Stop();
+await endpointInstance.Stop()
+    .ConfigureAwait(false);
